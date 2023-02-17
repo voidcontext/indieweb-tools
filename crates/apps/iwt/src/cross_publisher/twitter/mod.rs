95b4a9bad79d42a2af64430b49f9dad5b6c9b97b
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-02-17 17:59:55 +0000

    Try reducing tweet length, when twitter complains about its length

--- crates/apps/iwt/src/cross_publisher/twitter/mod.rs
+++ crates/apps/iwt/src/cross_publisher/twitter/mod.rs
@@ -1,7 +1,10 @@
 use std::rc::Rc;
 
+use crate::commons::permashort_link::PermashortCitation;
 use crate::commons::text;
+use crate::IwtError;
 use async_trait::async_trait;
+
 use futures::TryFutureExt;
 use oauth2::{basic::BasicClient, AuthUrl, ClientId, TokenUrl};
 use reqwest::Client;
@@ -58,6 +61,100 @@ struct TweetResponseData {
     id: String,
 }
 
+#[derive(serde::Deserialize)]
+struct TwitterErrorResponse {
+    errors: Vec<TwitterError>,
+}
+
+#[derive(serde::Deserialize)]
+struct TwitterError {
+    message: String,
+}
+
+impl<DB: TokenDB, USClient: url_shortener::Client> Twitter<DB, USClient> {
+    async fn try_publish<'a>(
+        &self,
+        post: &Item,
+        permashort_citation: &PermashortCitation,
+    ) -> Result<SyndicatedPost, Box<dyn std::error::Error + 'a>> {
+        let mut length = 280;
+        let mut success_or_gave_up = false;
+        let mut result = Err(Box::new(IwtError::new(
+            "You shouldn't see this, we're trying to publish now...",
+        )) as Box<dyn std::error::Error>);
+
+        while !success_or_gave_up {
+            let text = text::shorten_with_permashort_citation(
+                post.description().unwrap(),
+                length,
+                &permashort_citation,
+            );
+
+            let request = self
+                .http_client
+                .post("https://api.twitter.com/2/tweets")
+                .json(&TweetsRequest { text });
+
+            result =
+                self.authed_client
+                    .authed_request(request.build().unwrap())
+                    .and_then(|response| async {
+                        log::info!("Twitter response: {:?}", &response);
+
+                        let status = response.status();
+
+                        let body = response.text().await.expect("Body should be available");
+
+                        if status.is_success() {
+                            success_or_gave_up = true;
+                            serde_json::from_str::<TweetResponse>(&body)
+                                .map(|response| {
+                                    SyndicatedPost::new(Network::Twitter, &response.data.id, post)
+                                })
+                                .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
+                        } else {
+                            match serde_json::from_str::<TwitterErrorResponse>(&body) {
+                                Ok(error) => {
+                                    let too_long_error = error.errors.iter().any(|e| {
+                                        e.message.starts_with("Your Tweet text is too long.")
+                                    });
+
+                                    if too_long_error && length > 210 {
+                                        log::info!(
+                                            "Length {} was too long, trying to reduce it...",
+                                            length
+                                        );
+
+                                        Err(Box::new(IwtError::new(
+                                            "You shouldn't see this, we're trying it again",
+                                        ))
+                                            as Box<dyn std::error::Error>)
+                                    } else {
+                                        success_or_gave_up = true;
+                                        Err(Box::new(IwtError::new(
+                                        "Couldn't reliably reduce the length of the post, gave up",
+                                    ))
+                                        as Box<dyn std::error::Error>)
+                                    }
+                                }
+                                Err(err) => {
+                                    success_or_gave_up = true;
+                                    Err(Box::new(err) as Box<dyn std::error::Error>)
+                                }
+                            }
+                        }
+
+                        // log::info!("Twitter response body: {}", &body);
+                    })
+                    .await;
+
+            length = length - 5;
+        }
+
+        result
+    }
+}
+
 #[async_trait(?Send)]
 impl<DB: TokenDB, WHClient: url_shortener::Client> Target for Twitter<DB, WHClient> {
     async fn publish<'a>(
@@ -71,27 +168,7 @@ impl<DB: TokenDB, WHClient: url_shortener::Client> Target for Twitter<DB, WHClie
             .put_uri(post.link.as_ref().unwrap())
             .await?;
 
-        let text = text::shorten_with_permashort_citation(
-            post.description().unwrap(),
-            280,
-            &permashort_citation,
-        );
-
-        let request = self
-            .http_client
-            .post("https://api.twitter.com/2/tweets")
-            .json(&TweetsRequest { text });
-
-        self.authed_client
-            .authed_request(request.build().unwrap())
-            .and_then(|response| async {
-                let body = response.text().await.expect("Body should be available");
-
-                serde_json::from_str::<TweetResponse>(&body)
-                    .map(|response| SyndicatedPost::new(Network::Twitter, &response.data.id, post))
-                    .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
-            })
-            .await
+        self.try_publish(&post, &permashort_citation).await
     }
 
     fn network(&self) -> Network {
